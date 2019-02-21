485f86ab20cf1addb798b55cab34a1ea928b0c74
ningosi <nicotwendelee@gmail.com>
2019-02-21 09:05:36 +0300

    Making the metadata bundle a component

--- api/src/main/java/org/openmrs/module/southsudanemr/metadata/CommonMetadataBundle.java
+++ api/src/main/java/org/openmrs/module/southsudanemr/metadata/CommonMetadataBundle.java
@@ -1,7 +1,9 @@
 package org.openmrs.module.southsudanemr.metadata;
 
 import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommonMetadataBundle extends AbstractMetadataBundle {
 	
 	public void install() throws Exception {
