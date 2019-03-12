079b5062c99b80a66c43fdf98e65c64d8697e00a
ningosi <nicotwendelee@gmail.com>
2019-03-12 16:28:07 +0300

    Corrected the failing error in the sql statement

--- api/src/main/java/org/openmrs/module/southsudanemr/queries/Queries.java
+++ api/src/main/java/org/openmrs/module/southsudanemr/queries/Queries.java
@@ -7,7 +7,8 @@ public class Queries {
 	public static final String appointmentToday(int conceptId) {
 		return "SELECT pa.patient_id FROM patient pa INNER JOIN encounter e ON pa.patient_id=e.patient_id"
 		        + " INNER JOIN obs o ON pa.patient_id=o.person_id WHERE o.voided=0 AND o.concept_id=" + conceptId
-		        + " AND e.voided=0 AND o.value_datetime BETWEEN" + SsEmrReportUtils.formatDate(SsEmrReportUtils.getStartOfDay())
-		        + " AND " + SsEmrReportUtils.formatDate(SsEmrReportUtils.getEndOfDay());
+		        + " AND e.voided=0 AND o.value_datetime BETWEEN "
+		        + SsEmrReportUtils.formatDate(SsEmrReportUtils.getStartOfDay()) + " AND "
+		        + SsEmrReportUtils.formatDate(SsEmrReportUtils.getEndOfDay());
 	}
 }
